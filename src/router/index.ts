6d071e4ac40b27a92f4700ba2aba129db414f229
lahiruC22 <as2021356@sci.sjp.ac.lk>
2025-12-11 12:06:33 +0530

    modify the routes to point the returns page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
-    path: '/ledger',
-    name: 'ledger',
-    component: () => import('../views/BinCardLedgerView.vue'),
-  }],
+  routes: [
+    {
+      path: '/ledger',
+      name: 'ledger',
+      component: () => import('../views/BinCardLedgerView.vue'),
+    },
+    {
+      path: '/return',
+      name: 'return',
+      component: () => import('../views/ReturnView.vue'),
+    },
+  ],
 })
 
 export default router
