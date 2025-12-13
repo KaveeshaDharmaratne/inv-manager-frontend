a307826988e7a3c89ad71c7d54e9762202b75e03
Kaveesha Dharmaratne <76832073+KaveeshaDharmaratne@users.noreply.github.com>
2025-12-13 11:02:38 +0530

    code refactored

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,52 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+
+// In a real app, you would import actual components from ../views/
+const Placeholder = { template: '<div>Page Content</div>' };
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: '/stock/overview',
+      name: 'stock-overview',
+      component: Placeholder
+    },
+    {
+      path: '/stock/overview',
+      name: 'stock-overview',
+      component: Placeholder
+    },
+    {
+      path: '/stock/damage',
+      name: 'stock-damage',
+      component: Placeholder
+    },
+    {
+      path: '/transactions/sale',
+      name: 'transactions-sale',
+      component: Placeholder
+    },
+    {
+      path: '/transactions/return',
+      name: 'transactions-return',
+      component: Placeholder
+    },
+    {
+      path: '/reports/stock',
+      name: 'reports-stock',
+      component: Placeholder
+    },
+    {
+      path: '/reports/bin',
+      name: 'reports-bin',
+      component: Placeholder
+    },
+    {
+      path: '/reports/daily',
+      name: 'reports-daily',
+      component: Placeholder
+    },
+  ]
+});
 
-export default router
+export default router;
