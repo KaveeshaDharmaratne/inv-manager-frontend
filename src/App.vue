045ae988631fc24a78739d46243a9bc78e686140
KaveeshaDharmaratne <kaveeshadharmaratne@gmail.com>
2025-12-14 10:32:58 +0530

    style: format App

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,12 @@ import AppNavbar from './components/AppNavbar.vue'
 </script>
 
 <template>
-  <div class="flex h-screen bg-background-light dark:bg-background-dark text-zinc-800 dark:text-zinc-200 font-display">
-    <aside class="w-64 bg-white dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-800 flex flex-col">
+  <div
+    class="flex h-screen bg-background-light dark:bg-background-dark text-zinc-800 dark:text-zinc-200 font-display"
+  >
+    <aside
+      class="w-64 bg-white dark:bg-zinc-900 border-r border-zinc-200 dark:border-zinc-800 flex flex-col"
+    >
       <AppNavbar />
     </aside>
 
